195e9a8a20f5d4f8b413841395aba8af19e229f1
小羽 <821760648@qq.com>
2021-04-12 00:58:04 +0800

    update

--- reportList.js
+++ reportList.js
@@ -2,19 +2,19 @@
  * @description: 
  * @author: 小羽
  * @Date: 2021-03-21 11:18:32
- * @LastEditTime: 2021-03-23 15:37:04
+ * @LastEditTime: 2021-04-11 22:17:27
  * @Copyright: 1.0.0
  */
 let baseUrl = "https://my.sulg.top"
 let token = "Bearer%20eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0Y2I2ZjU5YTMzMWQ2ZTgwZDczNmI5Y2NiMmU4Yzk3IiwidHlwIjoiSldUIn0.eyJuYmYiOjE2MTY0NzQ1MDMsImV4cCI6MTYxNjUxMDUwMywiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAxIiwiYXVkIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NTAwMS9yZXNvdXJjZXMiLCJiYWlsdW5BcGkiXSwiY2xpZW50X2lkIjoiYmFpbHVuQ2xpZW50Iiwic3ViIjoi5Ya86bmP6aOeIiwiYXV0aF90aW1lIjoxNjE2NDc0NTAzLCJpZHAiOiJsb2NhbCIsIlVzZXJJZCI6IjI5MTgyMDMiLCJXeFVzZXJJZCI6InhpYW5wZW5nZmVpIiwic3VjZXNzcyI6InRydWUiLCJDb21wYW55Ijoie1wiSWRcIjoxLFwiQ29tcGFueUNvZGVcIjpcImJhaWx1blwiLFwiQ29tcGFueU5hbWVcIjpcIuW5v-W3nueZvuS8puS-m-W6lOmTvuenkeaKgOaciemZkOWFrOWPuFwifSIsIkFsbENvbXBhbnkiOiJGYWxzZSIsIlVzZXJDb2RlTmV3IjoiQkwxNzE3IiwiVXNlckNvZGUiOiJCTDU1NjY3NTAiLCJPYVVzZXJJZCI6IjMxOTgiLCJEZXBhcnRtZW50Ijoie1wiRGVwYXJ0bWVudElkXCI6MjYyLFwiTmFtZVwiOlwi5Lqn5ZOB6K6-6K6h57uEXCIsXCJDb2RlXCI6XCJcIn0iLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwiYmFpbHVuQXBpIl0sImFtciI6WyJjdXN0b20iXX0.f9MDLgVwP9bCYDkBot3izGObUTqQSNatQ9q_lqL1_LMitRnNz5wj2S9zGUunbk4o54SVjx293GdHknhJ5aVEJMVVUwLDFgLQAxSOOmbuxm69dbzxGran3sywuhYrl2uKT_WH-jHRuN44xZur8FemSGNcpwiYs4j5AnT9IRYvFFymeQF4dR0fnHxekPn0v3xI0KwzDpI4ZoP6--0OFT2AQxSK1Z_Raz3e6z_LwK6QPAF9B7gSfJxAiIrePkBcptLTJECtEjKIwVR9DLnSoAY8MDnkM48c5ZAZqWjKNqoP8aXNK13CNC1DY_QH3KmCyJZxAnxPGgG9X6yVi3bJXxvSuQ"
 module.exports = [
-  /* "https://www.baidu.com",
+  "https://www.baidu.com",
   `${baseUrl}`,
   `${baseUrl}/blogs/%E5%89%8D%E7%AB%AF/2020/vue%E7%9A%84%E6%80%A7%E8%83%BD%E4%BC%98%E5%8C%96.html`,
-  `http://ana.coupang.bailuntec.com/proDetails?token=${token}&&item_id=27613130` */
+  `http://ana.coupang.bailuntec.com/proDetails?token=${token}&&item_id=27613130`
 
   // shopify
-  "https://lagunamoon.com/",
+  /* "https://lagunamoon.com/",
   "https://lagunamoon.com/products/mood-essential-oil-bubble-blitz",
-  "https://lagunamoon.com/collections/carrior-oils"
+  "https://lagunamoon.com/collections/carrior-oils" */
 ]
